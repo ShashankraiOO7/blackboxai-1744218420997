361f46d0389d7cb789faba23a531532afeadfc77
Shashank Rai <shashankrai020700@gmail.com>
2025-04-09 22:37:30 +0530

    Add swagger.js 1744218449803

---
+++ swagger.js
@@ -0,0 +1,35 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'IRCTC Tatkal Booking API',
+      version: '1.0.0',
+      description: 'API for automated IRCTC Tatkal ticket booking',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server'
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
+    }
+  },
+  apis: ['./server.js']
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
